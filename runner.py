7e0ec2a2f35bd53557f275a8b0e3221a33ffb4f6
bigbangTay <bigbangTay@163.com>
2021-07-15 16:25:57 +0800

    'xiechengs'

---
+++ runner.py
@@ -0,0 +1,9 @@
+from xiecheng.Search.SearcH import Searchpage
+
+#from TestSuite.Variablelayer.Variable import *
+leave = '成都'
+leave_data = "2021-07-20"
+arrive = '北京'
+arrive_data = '2021-07-30'
+aa = Searchpage ()
+aa.search7 (leave = '成都',leave_data = "2021-07-20",arrive = '北京',arrive_data = '2021-07-30')
